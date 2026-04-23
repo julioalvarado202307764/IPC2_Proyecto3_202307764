342d62655c664f52f5352c0fcd7133c277452aae
Julio <3708774970101@ingenieria.usac.edu.gt>
2026-04-23 13:18:05 -0600

    boton y vista de ayuda

---
+++ Proyecto3_Razor/Pages/Ayuda.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto3_Razor.Pages
+{
+    public class AyudaModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Esta página es estática, solo se encarga de renderizar la vista.
+        }
+    }
+}
